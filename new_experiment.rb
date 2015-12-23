de829ec167db9d179d67fa7757def7705d265be7
Amanda Lilley <yoknitting24@gmail.com>
2015-12-23 18:07:57 -0500

    testing branch

---
+++ new_experiment.rb
@@ -0,0 +1,13 @@
+puts "What is the weather today?"
+weather = gets.chomp
+
+case weather
+when 'rainy'
+  puts "Get your rubber boots on and lets dance in the rain!"
+when 'cold'
+  puts "Finally! It is time to break out those awesome hand knits!"
+when 'sunny'
+  puts "Grab your bikini!"
+when 'windy'
+  puts "Hold your hat!"
+end
